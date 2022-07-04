36d71870b641744dcba09291b327fcbd1be2c2b7
chance <changxunzhou@qq.com>
2022-07-04 16:25:01 +0800

    Update select_mode_echo_server.py

--- select_mode_echo_server.py
+++ select_mode_echo_server.py
@@ -3,8 +3,12 @@ import json
 import socket
 import select
 
+prompt = 'server> '
+welcome = 'welcome to here!\n'
+
 def server_loop():
     sk = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # 创建一个套接字对象
+    sk.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
     ipaddr = ('127.0.0.1', 9998)  # ip 和端口号
     sk.bind(ipaddr)   # 绑定
     sk.listen(5)   # 监听
@@ -20,21 +24,33 @@ def server_loop():
             if r == sk:
                 conn, addr = r.accept()
                 print('new connection from {}'.format(addr))
-                conn.sendall(b'hello boy!')
+                conn.sendall((welcome+prompt).encode())
                 inputs.append(conn)
                 massages[conn] = []
             else:  # read
                 try:
-                    data = r.recv(1024).decode(encoding='utf-8')
+                    data = r.recv(1024)
                     if not data:
                         print('disconnect from {}'.format(r.getpeername()))
                         raise Exception('disconnect')
-                except Exception as e: # 异常处理，防止客户端突然断开然后 程序保存崩溃
+                except Exception as e: # 异常处理，防止客户端突然断开然后 程序崩溃
                     inputs.remove(r)
                     del massages[r]
                     continue
 
-                print('recv from {}: [{}]'.format(r.getpeername(), data))
+                print('<< {}: [{}]'.format(r.getpeername(), data))
+                if data == b'\xff\xf4\xff\xfd\x06':  # telnet client pressed Ctrl-C
+                    print('telnet client {} leave.'.format(r.getpeername()))
+                    r.close()
+                    inputs.remove(r)
+                    del massages[r]
+                    continue
+                try:
+                    data = data.decode(encoding='utf-8')
+                except Exception as e:
+                    print(e)
+                    data = 'uft-8 data is required.'
+
                 if data.strip() == 'q':
                     r.close()
                     inputs.remove(r)
@@ -43,13 +59,14 @@ def server_loop():
                 elif data.strip() == 'who':
                     data = json.dumps(['{}:{}'.format(*conn.getpeername()) for conn in inputs if conn != sk], indent=2)
                 outputs.append(r)
-                massages[conn].append(data)
+                massages[r].append(data)
 
         for w in wlist:  # write
             msg = massages[w].pop()
-            buf = ('response:'+msg).encode(encoding='utf-8')
-            print('send to   {}: [{}]'.format(w.getpeername(), buf))
+            buf = (msg+prompt).encode(encoding='utf-8')
+            print('>> {}: [{}]'.format(w.getpeername(), buf))
             w.sendall(buf)
             outputs.remove(w)
+
 
 server_loop()
